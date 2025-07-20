6bec0e4243c82c073d08a012f3f8025283e563be
edrafferty <ourgoalie@gmail.com>
2025-07-20 14:01:08 -0300

    Fix: remove explicit any from leaflet icon override

--- components/InteractiveMap.tsx
+++ components/InteractiveMap.tsx
@@ -5,7 +5,7 @@ import L, { LatLngExpression } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
 // Fix Leaflet marker icon paths for Next.js
-delete (L.Icon.Default.prototype as any)._getIconUrl
+delete (L.Icon.Default.prototype as { _getIconUrl?: () => string })._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
   iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
