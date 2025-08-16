6a28072cfbd0c8240060c19f0cf2169b6195f2e2
edrafferty <ourgoalie@gmail.com>
2025-08-16 20:49:28 -0300

    reorganized partners

--- data/partners.ts
+++ data/partners.ts
@@ -8,6 +8,12 @@ export interface Partner {
 }
 
 export const partners: Partner[] = [
+   {
+    name: "Atlantic Canada Bulk Terminal",
+    description: "Atlantic Canada Bulk Terminal expertly handles international cargo requirements at a competitive price. Committed to client satisfaction, our team provides efficient and timely stevedoring, terminal, and warehouse services.",
+    logo: "/partners/acbt.png",
+    website: "https://acbt.ca",
+  },
   {
     name: "Atlantic Pilotage Authority",
     description: "The APA works with ports, the shipping industry, and other stakeholders to provide the safest and most efficient marine pilotage service possible to Atlantic Canada.",
@@ -20,23 +26,19 @@ export const partners: Partner[] = [
     logo: "/partners/atl.png",
     website: "https://www.atlantictowing.com",
   },
+{
+    name: "Halifax Port Authority",
+    description: "A strategic port manager, with a mission to connect with global markets to create value for our customers, partners, visitors, and community.",
+    logo: "/partners/hpa-logo.png",
+    website: "https://porthalifax.ca",
+  },
   {
     name: "Svitzer",
     description: "A global provider of towage and marine services, specializing in assisting large vessels with maneuvering in and out of ports and terminals",
     logo: "/partners/Svitzer-Logo.png",
     website: "https://www.svitzer.com",
   },
-  {
-    name: "Atlantic Canada Bulk Terminal",
-    description: "Atlantic Canada Bulk Terminal expertly handles international cargo requirements at a competitive price. Committed to client satisfaction, our team provides efficient and timely stevedoring, terminal, and warehouse services.",
-    logo: "/partners/acbt.png",
-    website: "https://acbt.ca",
-  },
-   {
-    name: "Halifax Port Authority",
-    description: "A strategic port manager, with a mission to connect with global markets to create value for our customers, partners, visitors, and community.",
-    logo: "/partners/hpa-logo.png",
-    website: "https://porthalifax.ca",
-  },
+ 
+
   // Add more as needed
 ]
